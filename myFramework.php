83b982e0812caf3c63849e18ff46502e2d473e28
Kaiser Waseem <kaiser.waseem@logicait.pk>
2015-04-21 18:26:12 +0500

    initial entries

---
+++ myFramework.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of myFramework
+ *
+ * @author Kaiser Waseem {kaiserwaseem@gmail.com}
+ * Dated:  April 21, 2015
+ * 
+ */
+class myFramework extends myFrameworkBase {
+    //put your code here
+}
---
+++ myFrameworkBase.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Description of myFrameworkBase
+ *
+ * @author Kaiser Waseem {kaiserwaseem@gmail.com}
+ * Dated:  April 21, 2015
+ * 
+ */
+class myFrameworkBase {
+    //put your code here
+}
